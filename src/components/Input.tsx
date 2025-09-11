6c76709e8c8e164c75d1bd99ff972fa0f8560e31
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-10 22:01:28 -0300

    feat: input component ccreated

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { InputHTMLAttributes, useId } from "react"
+
+import styles from "./style.module.css"
+
+type PropsInput = InputHTMLAttributes<HTMLInputElement> & {
+  label: string
+  name: string
+  error?: string | undefined
+}
+
+export default function Input({label, error, ...props}: PropsInput){
+  const id = useId();
+  console.log("error: ", error)
+  return (
+  <div className={styles.inputContainer}>
+    <label htmlFor={id}>{label}</label>
+    <input className={styles.input} id={id} {...props}/>
+    {error && <p className={styles.errorAlert}>{error}</p>}
+  </div>
+  )
+}
