16ef5f0548ae56401477ebaafe268d869c8ac4d5
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-10 23:52:38 -0300

    feat: request validation and router created

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,11 +2,14 @@
 
 import { SubmitHandler, useForm } from "react-hook-form"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 import Input from "@/components/Input"
 import { passwordRules, usernameRules } from "./const"
 
 import styles from "../style.module.css"
+import config from "@/config"
+import axios from "axios"
 
 type Form = {
   username: string
@@ -14,9 +17,32 @@ type Form = {
 }
 
 export default function Login() {
-  const { register, handleSubmit, formState: { errors } } = useForm<Form>();
-  const onSubmit: SubmitHandler<Form> = (data) => {
-    console.log("clicado", data)
+  const { register, handleSubmit, setError, formState: { errors } } = useForm<Form>();
+  const router = useRouter()
+  const onSubmit: SubmitHandler<Form> = async (data) => {
+    try {
+      const response = await config.login(data.username, data.password)
+      const token = response?.data?.access_token;
+      if (token) {
+        localStorage.setItem("token", token);
+        router.push("/home");
+      }
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        const error = e?.response?.data
+        if (error.message === "username not found") {
+          return setError("username", { type: "manual", message: "Usuário não encontrado" })
+        } else if (error.message === "Incorrect password") {
+          return setError("password", { type: "manual", message: "Senha incorreta" })
+        }else{
+          console.log(e)
+          alert("Um erro desconhecido aconteceu, contate o desenvolvedor")  
+        }
+      }else{
+        console.log(e)
+        alert("Um erro desconhecido aconteceu, contate o desenvolvedor")
+      }
+    }
   };
 
   return (
@@ -27,11 +53,11 @@ export default function Login() {
         label="Usuário: "
         placeholder="meu_usuario"
         error={errors?.username?.message}
-        {...register("username", usernameRules)} 
+        {...register("username", usernameRules)}
       />
-      
-      <Input 
-        label="Senha: " 
+
+      <Input
+        label="Senha: "
         placeholder="123456"
         error={errors?.password?.message}
         {...register("password", passwordRules)}
@@ -39,7 +65,7 @@ export default function Login() {
 
       <button type="submit" className="btn success">Login</button>
       <Link href={"/register"}>Não possui conta? Cadastre-se.</Link>
-      
+
     </form>
   )
 }
