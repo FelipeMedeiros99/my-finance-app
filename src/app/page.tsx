59f97d02cc9b1ba7f467a00e69731c6cf4127b6b
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-10 22:03:44 -0300

    feat: redirect to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+"use client"
+
+import { useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 
 import styles from "./style.module.css"
+import { useRouter } from "next/navigation";
 
 
 export default function Start() {
+  const route = useRouter()
+  useEffect(() => {
+    route.push("/login");
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <ClipLoader size="100" color="#16A34A"/>
+    <div className={styles.spinnerContainer}>
+      <ClipLoader size="100" color="#16A34A" />
     </div>
   );
 }
