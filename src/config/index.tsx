91de3861276d671e8f7d4d09b97c342b4984068b
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-10 23:50:53 -0300

    feat: axios config created

---
+++ src/config/index.tsx
@@ -0,0 +1,28 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+class AxiosConfig {
+  instance;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: "http://localhost:5000",
+    })
+
+    this.instance.interceptors.request.use((config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+  }
+
+  async login(username: string, password: string): Promise<AxiosResponse> {
+    return await this.instance.post("/login", { username, password })
+  }
+
+}
+
+const config = new AxiosConfig();
+
+export default config;
