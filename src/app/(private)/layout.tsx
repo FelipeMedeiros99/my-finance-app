8241058dfac0165906d58cec87c89ac891d73798
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-13 02:39:30 -0300

    feat: pages created

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,16 +1,20 @@
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 
+import styles from "./style.module.css"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <>
       <Header />
-      {children}
+      <main className={styles.main}>
+        {children}
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
---
+++ src/app/(private)/transfers/page.tsx
@@ -0,0 +1,5 @@
+export default function Transfers(){
+  return(
+    <p>transfers</p>
+  )
+}
