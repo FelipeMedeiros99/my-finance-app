c93616c8e6800834fe6bbbe6c5723505a1f31dcf
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-16 20:27:51 -0300

    feat: category page and functions created

--- src/app/(private)/category/page.tsx
+++ src/app/(private)/category/page.tsx
@@ -1,5 +1,153 @@
-export default function Category(){
-  return(
-    <p>category</p>
+"use client"
+
+import React, { useEffect, useState } from "react"
+import { FaEdit, FaPlusCircle } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import config from "@/config"
+
+import WhiteContainer from "@/components/white-container/WhiteContainer"
+import Input from "@/components/input/Input";
+
+import styles from "./style.module.css"
+
+type Form = {
+  name: string;
+  type: string;
+  id: number | null;
+}
+
+export default function Category() {
+  const [categories, setCategories] = useState([])
+  const [pageType, setPageType] = useState<"EXPENSE" | "INCOME">("EXPENSE")
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const { register, setValue, setError, handleSubmit, formState: { errors } } = useForm<Form>({
+    defaultValues: {
+      name: "",
+      type: pageType,
+      id: null
+    }
+  })
+
+  const onSubmit: SubmitHandler<Form> = async (data: Form) => {
+    try{
+      if(!data.id){
+        await config.createCategory(data.name, data.type)
+        await getCategorys()
+        cancelModalFunction()
+      }else{
+        await config.editCategory(data.id, data.name)
+        await getCategorys()
+        cancelModalFunction()
+      }
+    }catch(e){
+      console.log(e)
+      alert("Erro ao tentar criar a categoria")
+    }
+  }
+
+  const openModal = () => {
+    setIsModalVisible(true);
+    setValue("name", "");
+    setValue("type", pageType);
+    setValue("id", null);
+    setError("name", {type: "required", message: ""}) 
+  }
+
+
+  const getCategorys = async () => {
+    try {
+      const categorys = await config.getCategorys()
+      setCategories(categorys.data);
+    } catch (e) {
+      console.log(e)
+      alert("Erro ao tentar buscar as categorias")
+    }
+  }
+
+  const deleteCategorys = async (id: number) => {
+    try {
+      await config.deleteCategory(id)
+      await getCategorys()
+    } catch (e) {
+      console.log(e)
+      alert("Erro ao tentar deletar a categoria")
+    }
+  }
+
+  const cancelModalFunction = () => {
+    setIsModalVisible(false);
+    setValue("name", "");
+    setValue("type", pageType);
+    setValue("id", null);
+    setError("name", {type: "required", message: ""});
+  }
+
+  const editCategory = (category: any) => {
+    setValue("name", category.name);
+    setValue("type", category.type);
+    setValue("id", category.id);
+    setIsModalVisible(true);
+  }
+
+  useEffect(() => {
+    (async () => {
+      await getCategorys()
+    })()
+  }, [])
+
+  return (
+    <React.Fragment>
+
+      <div className={styles.containerButtons}>
+        <button onClick={() => { setPageType("EXPENSE") }} className="btn danger">Despesas</button>
+        <button onClick={() => { setPageType("INCOME") }} className="btn success">Receitas</button>
+      </div>
+
+      <WhiteContainer title="Categorias" theme={pageType === "EXPENSE" ? "red" : pageType === "INCOME" ? "green" : "neutral"}>
+
+        <FaPlusCircle className={styles.icon} onClick={() => openModal()}/>
+
+        <div className={styles.categoryContainer}>
+          {categories &&
+            categories.map((category: any) => {
+              {
+                if (category.type === pageType) {
+                  return (
+                    <div key={category.id} className={styles.category}>
+                      <p>{category.name}</p>
+                      <div className={styles.icons}>
+                        <FaEdit onClick={() => editCategory(category)}/>
+                        <AiFillDelete onClick={() => deleteCategorys(category.id)} />
+                      </div>
+                    </div>
+                  )
+                }
+              }
+            })}
+        </div>
+
+
+
+        {isModalVisible &&
+
+          <div className={styles.modal}>
+            <WhiteContainer title="Nova categoria" theme={pageType === "EXPENSE" ? "red" : pageType === "INCOME" ? "green" : "neutral"}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+
+                <Input {...register("name", { required: {value: true, message: "Campo obrigatório"} })} error={errors.name?.message} label="Título: " placeholder="Título da categoria" />
+
+                <div className={styles.containerButtons}>
+                  <button type="button" onClick={cancelModalFunction} className="btn danger">Cancelar</button>
+                  <button type="submit" className="btn success">Salvar</button>
+                </div>
+              </form>
+
+            </WhiteContainer>
+          </div>}
+      </WhiteContainer>
+    </React.Fragment>
   )
 }
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -5,7 +5,7 @@ class AxiosConfig {
 
   constructor() {
     this.instance = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: "http://localhost:5002",
     })
 
     this.instance.interceptors.request.use((config) => {
@@ -23,6 +23,22 @@ class AxiosConfig {
 
   async register(username: string, password: string, confirmPassword: string){
     return await this.instance.post("/register", {username, password, confirmPassword})
+  }
+
+  async getCategorys(){
+    return await this.instance.get("/category/all")
+  }
+
+  async deleteCategory(id: number){
+    return await this.instance.delete(`/category/${id}`)
+  }
+
+  async createCategory(name: string, type: string){
+    return await this.instance.post("/category", {name, type})
+  }
+
+  async editCategory(id: number, name: string){
+    return await this.instance.put(`/category/${id}`, {name})
   }
 
 }
