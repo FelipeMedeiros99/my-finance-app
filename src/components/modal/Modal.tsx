1edff114650e7b2e180ec14eab760330e50bfc56
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-16 21:52:44 -0300

    feat: modal component created

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { useEffect, useRef } from "react";
+import WhiteContainer from "../white-container/WhiteContainer";
+
+import styles from "./style.module.css";
+
+
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  title?: string;
+  theme?: "green" | "red" | "neutral"
+}
+
+export default function Modal({ children, isOpen, setIsOpen, theme }: Props) {
+  const modalRef = useRef<HTMLDivElement>(null)
+  
+  useEffect(()=>{
+    const handleClickOutside = (event: MouseEvent) => {
+      if (isOpen){
+        if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+          setIsOpen(false);
+        }
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return (()=>{
+      document.removeEventListener("click", handleClickOutside);
+    })
+  }, [isOpen])
+  
+  return (
+    <React.Fragment>
+      {isOpen &&
+        <div className={styles.modal}>
+          <div ref={modalRef}>
+          <WhiteContainer title="Nova categoria" theme={theme}>
+            {children}
+          </WhiteContainer>
+          </div>
+        </div>
+      }
+    </React.Fragment>
+  )
+
+}
