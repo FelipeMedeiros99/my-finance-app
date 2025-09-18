d2ee092d6f0d32bb36a4fae629160ba755b04ece
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-18 16:40:14 -0300

    feat: spinner added

--- src/components/white-container/WhiteContainer.tsx
+++ src/components/white-container/WhiteContainer.tsx
@@ -2,19 +2,31 @@
 
 import React from "react";
 
+import { ClipLoader } from "react-spinners";
+
 import styles from "./style.module.css";
 
 type Props = {
   children?: React.ReactNode;
   title: string;
-  theme?: "green" | "red" | "neutral"
+  theme?: "green" | "red" | "neutral",
+  isLoading?: boolean;
 }
 
-export default function WhiteContainer({children, theme="neutral", title}: Props){
-  return(
-    <div className={`${styles.container} ${styles[theme]}`}>
-      <h3 className={styles.title}>{title}</h3>
-      {children}
-    </div>
+export default function WhiteContainer({ children, theme = "neutral", title, isLoading = false }: Props) {
+  return (
+    <>
+      {isLoading &&
+        <div className={styles.spinnerContainer}>
+          <ClipLoader size="100" color="#16A34A" />
+        </div>
+      }
+      {!isLoading &&
+        <div className={`${styles.container} ${styles[theme]}`}>
+          <h3 className={styles.title}>{title}</h3>
+          {children}
+        </div>
+      }
+    </>
   )
 }
