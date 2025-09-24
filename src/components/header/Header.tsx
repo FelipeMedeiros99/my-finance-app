b8e679da1548cbb700416e07af9833fad5f13c0a
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-23 22:32:30 -0300

    feat: header and bottom added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,22 @@
 "use client"
 
 import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { AiOutlineMenu } from "react-icons/ai";
+// import { IoIosLogOut } from "react-icons/io";
 
 import { routesSettings } from "./const";
 
 import styles from "./style.module.css"
 
+
 export default function Header() {
   const [isLinksVisible, setIsLinksVisible] = useState(false)
   const path = usePathname()
   const ulRef = useRef<HTMLUListElement | null>(null)
+  const route = useRouter()
 
   useEffect(() => {
     const closeLinks = (e: MouseEvent) => {
@@ -28,22 +32,45 @@ export default function Header() {
     }
   }, [isLinksVisible])
 
+  const exit = () => {
+    localStorage.removeItem("token");
+    route.push("/");
+  }
+
+
   return (
     <header className={styles.header}>
-      <AiOutlineMenu className={styles.icon} onClick={() => setIsLinksVisible(!isLinksVisible)} />
 
-        <nav className={`${styles.nav} ${!isLinksVisible ? styles.hiddem : ""}`}>
-          <ul ref={ulRef} className={styles.ul}>
-            {routesSettings.map((routerSetting) => (
-              <Link 
-                onClick={()=>setIsLinksVisible(false)}
-                className={routerSetting.route===path ? `${styles.link} ${styles.linkActive}` : `${styles.link}`} 
-                href={routerSetting.route} 
-                key={routerSetting.route}>{routerSetting.label}
-              </Link>
-            ))}
-          </ul>
-        </nav>
+      <AiOutlineMenu className={styles.iconOpen} onClick={() => setIsLinksVisible(!isLinksVisible)} />
+
+      <nav className={`${styles.nav} ${!isLinksVisible ? styles.hiddem : ""}`}>
+
+        <div className={styles.headerNav}>
+          <div className={styles.containerLogo}>
+            <Image src="/images/logo.png" alt="logo" fill></Image>
+          </div>
+          <h2>Finanças</h2>
+        </div>
+
+        <ul ref={ulRef} className={styles.ul}>
+          {routesSettings.map((routerSetting) => (
+            <Link
+              onClick={() => setIsLinksVisible(false)}
+              className={routerSetting.route === path ? `${styles.link} ${styles.linkActive}` : `${styles.link}`}
+              href={routerSetting.route}
+              key={routerSetting.route}>{routerSetting.label}
+            </Link>
+          ))}
+        </ul>
+
+        <div className={styles.footer}>
+          <button onClick={exit}>
+            Sair
+            {/* <IoIosLogOut/> */}
+            </button>
+        </div>
+
+      </nav>
     </header>
   )
 }
