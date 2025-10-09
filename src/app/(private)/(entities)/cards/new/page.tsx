4d8c2b659554f4414e8009980a575384b6910812
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-09 01:15:29 -0300

    cleanup: removed archive

--- src/app/(private)/(entities)/cards/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import WhiteContainer from "@/components/white-container/WhiteContainer";
-
-export default function New(){
-  return(
-    <WhiteContainer title="Nova despesa">
-
-    </WhiteContainer>
-  )
-}
