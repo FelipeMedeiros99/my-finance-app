5ef9f92999123d6446eca25a208369cb87b4c9ef
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-22 14:28:52 -0300

    feat: logo added

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -1,20 +1,30 @@
 import { FormHTMLAttributes } from "react";
+import Image from "next/image";
+
+import logo from "@/assets/images/logo.png"
 
 export default function AuthContainer({ children, ...props }: FormHTMLAttributes<HTMLFormElement>) {
   return (
     <form className="
       container
-      grid grid-cols-1 
-      gap-6 
       px-8 py-5 
       border border-gray-300 rounded-2xl
       w-full max-w-80
-      justify-items-center
       shadow-sm
       bg-white
+      grid-cols-1
+      justify-items-center
       " {...props}
-    >
+      >
+      <Image src={logo} alt="Logo do Financial Control" className="h-15 w-15"/>
+      <div className="
+      grid grid-cols-1 
+      gap-6 
+      justify-items-center
+
+      ">
       {children}
+      </div>
     </form>
   )
 }
