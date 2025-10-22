8dcd98d53039d7ec51c99e3607998bbad6f2b326
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-22 10:09:45 -0300

    fix: effect readded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ import { useRouter } from "next/navigation";
 
 export default function Start() {
   const route = useRouter()
-  // useEffect(() => {
-  //   route.push("/login");
-  // }, [route])
+  useEffect(() => {
+    route.push("/login");
+  }, [route])
 
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-green-600">
