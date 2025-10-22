483c5c8ea1eaf8a1206fdc2e07adb63972c5b15d
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-22 12:57:29 -0300

    refactor: tailwind added

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -2,8 +2,6 @@
 
 import { InputHTMLAttributes, useId } from "react"
 
-import styles from "./style.module.css"
-
 type PropsInput = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   name?: string
@@ -13,10 +11,24 @@ type PropsInput = InputHTMLAttributes<HTMLInputElement> & {
 export default function Input({label, error, ...props}: PropsInput){
   const id = useId();
   return (
-  <div className={styles.inputContainer}>
+  <div>
     <label htmlFor={id}>{label}</label>
-    <input className={styles.input} id={id} {...props}/>
-    {error && <p className={styles.errorAlert}>{error}</p>}
+    <input
+          className='
+            shadow-lg
+            border border-gray-400
+            px-2 py-1
+            rounded-md
+            focus:outline-none focus:ring-1 focus:ring-lime-600 
+          '
+          {...props}
+          />
+    <p className='
+          mt-1
+          text-xs
+          text-red-500
+        '
+        >{error}</p>
   </div>
   )
 }
