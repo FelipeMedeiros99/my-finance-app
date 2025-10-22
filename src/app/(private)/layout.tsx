5c7a56274e54f155811607303c8d48788c0029f6
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-22 19:30:04 -0300

    cleanup: unused component removed

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -2,8 +2,6 @@ import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import Guard from "@/components/guard/guard";
 
-import styles from "./style.module.css"
-
 export default function RootLayout({
   children,
 }: Readonly<{
