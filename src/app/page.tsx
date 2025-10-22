1c27a40ba7bc6e6f23009ab44f06922a908e4cfa
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-22 10:07:48 -0300

    refactor: tailwind added to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ import { useRouter } from "next/navigation";
 
 export default function Start() {
   const route = useRouter()
-  useEffect(() => {
-    route.push("/login");
-  }, [route])
+  // useEffect(() => {
+  //   route.push("/login");
+  // }, [route])
 
   return (
-    <div className={styles.spinnerContainer}>
-      <ClipLoader size="100" color="#16A34A" />
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-green-600">
+      <ClipLoader size={100} color="#16A34A" />
     </div>
   );
 }
